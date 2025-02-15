96c336d0d49c732ae7b61eff98377c85289e6620
iaschowrai <iaschowrai@gmail.com>
2025-02-15 17:45:41 -0500

    Initial Commit

---
+++ src/main/java/com/githubaction/github_action_build_deploy/GithubActionBuildDeployApplication.java
@@ -0,0 +1,13 @@
+package com.githubaction.github_action_build_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionBuildDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionBuildDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubaction/github_action_build_deploy/GithubActionBuildDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubaction.github_action_build_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionBuildDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
